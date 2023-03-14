895e9355b56a875abe1eeb21e4d1508a59b9c3b7
chanpod <chanpod36@gmail.com>
2023-03-14 18:03:09 -0400

    loging callback is in env now

--- app/server/auth/strategies/googleStrategy.ts
+++ app/server/auth/strategies/googleStrategy.ts
@@ -5,7 +5,7 @@ export let googleStrategy = new GoogleStrategy(
     {
         clientID: "1034070132753-gv1tbh0sop6rr0sqlst0e6rcp8ajdv0n.apps.googleusercontent.com",
         clientSecret: "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",
-        callbackURL: "http://localhost:3000/login/google",
+        callbackURL: process.env.GOOGLE_REDIRECT_URI ?? "http://localhost:3000/login/google",
     },
     async ({ accessToken, refreshToken, extraParams, profile }) => {
         console.log(profile);
